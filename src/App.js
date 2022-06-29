45a05fb1812e63783c0ec8c3bce4625399920294
pqx <l.r.slegt@gmail.com>
2022-06-29 15:48:34 +0200

    add FunctionClick.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
+      {/* <Message /> */}
       {/* <Greet name="Bruce" heroName="Batman"><p>This is children props</p></Greet>
-      <Greet name="Clark" heroName="Superman"><button>Action</button></Greet>
+      <Greet name="Clark" heroName="Superman"><button>Action</button></Greet> */}
       <Greet name="Diana" heroName="Wonder Woman" />
       <Welcome name="Bruce" heroName="Batman" />
-      <Welcome name="Clark" heroName="Superman" />
+      {/* <Welcome name="Clark" heroName="Superman" />
       <Welcome name="Diana" heroName="Wonder Woman" /> */}
       {/* <Hello /> */}
     </div>
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,13 +4,13 @@ import { Divider } from '@material-ui/core';
 import React from 'react';
 
 const Greet = (props) => {
-  console.log(props);
+  const { name, heroName } = props;
   return (
     <div>
       <h1>
-        Hello {props.name} a.k.a. {props.heroName}
+        Hello {name} a.k.a. {heroName}
       </h1>
-      {props.children}
+      props.children
     </div>
   );
 };
