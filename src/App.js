77f5f93dffc06331015e873ebd6fb2edec8509d7
pqx <l.r.slegt@gmail.com>
2022-06-30 11:31:32 +0200

    add EventBind.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
-import Message from './components/Message'
-import Counter from './components/Counter'
+import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <EventBind />
+      {/* <FunctionClick />
+      <ClassClick /> */}
+      {/* <Counter /> */}
       {/* <Message /> */}
       {/* <Greet name="Bruce" heroName="Batman"><p>This is children props</p></Greet>
       <Greet name="Clark" heroName="Superman"><button>Action</button></Greet> */}
-      <Greet name="Diana" heroName="Wonder Woman" />
-      <Welcome name="Bruce" heroName="Batman" />
+      {/* <Greet name="Diana" heroName="Wonder Woman" />
+      <Welcome name="Bruce" heroName="Batman" /> */}
       {/* <Welcome name="Clark" heroName="Superman" />
       <Welcome name="Diana" heroName="Wonder Woman" /> */}
       {/* <Hello /> */}
